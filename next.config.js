ecdf5496fbe281d95731bbac7192a9950fbdead3
tomo <tomomasa0108@gmail.com>
2025-03-02 16:34:27 +0900

    PWAの実装２

--- next.config.js
+++ next.config.js
@@ -1,26 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+});
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['raw.githubusercontent.com'],
-    unoptimized: true,
   },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  experimental: {
-    optimizeCss: false, // CSS最適化を無効化
-  },
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.watchOptions = {
-        poll: 1000,
-        aggregateTimeout: 300,
-      }
-      config.cache = false;
-    }
-    return config;
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
